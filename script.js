e6ea150e964f069102ca3981d623e1d1aba49382
José Wanderson <123521324+wanderson-prog@users.noreply.github.com>
2023-03-02 19:39:40 -0300

    Atualizei

--- script.js
+++ script.js
@@ -6,28 +6,26 @@ function relogioDigital() {
     let imagem = document.getElementById('imagem')
     
     if (hora >= 5 && hora < 12) {
-        //Bom Dia
         imagem.src = 'imagem/manha.png'
         document.body.style.background = '#ECDBC8'
     } else if (hora >= 12 && hora <= 18) {
-        //Boa Tarde
         imagem.src = 'imagem/tarde.png'
         document.body.style.background = '#E68F71'
     } else if (hora <= 24 || hora < 5) {
-        //Boa Noite
         imagem.src = 'imagem/noite.png'
         document.body.style.background = '#2D6AC2'
     }
 
     if (hora < 10) {
-        hora = '0' + hora
+        hora = `0${hora}`
     }
     if (minuto < 10) {
-        minuto = '0' + minuto
+        minuto = `0${minuto}`
     }
     if (segundo < 10) {
-        segundo = '0' + segundo
+        segundo = `0${segundo}`
     }
+    
     document.getElementById('horas').innerHTML = hora
     document.getElementById('minutos').innerHTML = minuto
     document.getElementById('segundos').innerHTML = segundo
